91a6714da3049dfff9f98879d5536cde33de6e11
Andy Huynh <115195444+andyh10@users.noreply.github.com>
2025-09-23 14:07:11 -0700

    Update client.py
    Added input for the socket binding

--- client.py
+++ client.py
@@ -1,9 +1,32 @@
 import socket
+import ipaddress
 
-# Create socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+def main():
+    # Create socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# Bind to the local IP address and UDP port
+    # Bind to the local IP address and UDP port
+    while True:
+        ip_addr = input("Please input your device's IP: ").strip()
+    
+        # Make sure that the IP address is correct.
+        try:
+            ipaddress.ip_address(ip_addr)
+            break
+        except ValueError:
+            print("Invalid IP address input.")
 
-# Send data
+    while True:
+        port_num = int(input("Please input the port number you want to bind: "))
 
+        # Make sure it is in the right port range.
+        if port_num >= 13100 and port_num <= 13199:
+            break
+        else:
+            print("Please use a port number in the range of 13100-13199.")  
+
+    sock.bind((ip_addr, port_num))
+
+    # Send data
+
+main()
