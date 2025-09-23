6c057af0679cde95adb5baa662c498b567ecd426
Andy Huynh <115195444+andyh10@users.noreply.github.com>
2025-09-23 16:08:43 -0700

    Update client.py
    Changed it so that the input is from the cmd-line and not keyboard input.

--- client.py
+++ client.py
@@ -1,54 +1,39 @@
 import socket
 import ipaddress
+import sys
 
 def main():
+    if len(sys.argv) != 4:
+        print("Please make sure you have the correct arguments.")
+        print("client.py <client_port> <server_ip> <server_port>")
+        exit(-1)
+
+    if int(sys.argv[1]) < 13100 or int(sys.argv[1]) > 13199:
+        print("Client Port - Please use a port number in the range of 13100-13199.")
+        exit(-1)
+
+    try:
+        ipaddress.ip_address(sys.argv[2])
+    except ValueError:
+        print("Invalid server IP address")
+        exit(-1)
+
+    if int(sys.argv[3]) < 13100 or int(sys.argv[3]) > 13199:
+        print("Server Port - Please use a port number in the range of 13100-13199.")
+        exit(-1)
+
     # Create socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     # Bind to the local IP address and UDP port
-    while True:
-        ip_addr = input("Please input your device's IP: ").strip()
-    
-        # Make sure that the IP address is correct.
-        try:
-            ipaddress.ip_address(ip_addr)
-            break
-        except ValueError:
-            print("Invalid IP address input.")
+    sock.bind(('127.0.0.1', int(sys.argv[1])))
 
-    while True:
-        port_num = int(input("Please input the port number you want to bind: "))
+    # Send data to server 
+    message = input("Please type the command you want to send to the manager: ")
+    sock.sendto(message.encode("utf-8"), (sys.argv[2], int(sys.argv[3])))
 
-        # Make sure it is in the right port range.
-        if port_num >= 13100 and port_num <= 13199:
-            break
-        else:
-            print("Please use a port number in the range of 13100-13199.")  
-
-    sock.bind((ip_addr, port_num))
-
-    # Send data to server
-    while True:
-        ip_addr_manager = input("Please input the manager process's IP: ").strip()
-    
-        # Make sure that the IP address is correct.
-        try:
-            ipaddress.ip_address(ip_addr_manager)
-            break
-        except ValueError:
-            print("Invalid IP address input.")
-    
-    while True:
-        port_num_manager = int(input("Please input manager process's port: "))
-
-        # Make sure it is in the right port range.
-        if port_num_manager >= 13100 and port_num_manager <= 13199:
-            break
-        else:
-            print("Please use a port number in the range of 13100-13199.") 
-
-    message = input("Please type the command you want to send to the manager")
-    sock.sendto(message.encode('utf-8'), (ip_addr_manager, port_num_manager))
+    data, addr = sock.recvfrom(1024)
+    print(f"Server Response: {data.decode('utf-8')}")
 
     sock.close()
     
