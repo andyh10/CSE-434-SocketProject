07510c7b34db15e2ec61a832e512d59b04a04264
Andy Huynh <115195444+andyh10@users.noreply.github.com>
2025-09-23 14:21:22 -0700

    Update client.py
    Added send message to server functionality

--- client.py
+++ client.py
@@ -27,6 +27,29 @@ def main():
 
     sock.bind((ip_addr, port_num))
 
-    # Send data
+    # Send data to server
+    while True:
+        ip_addr_manager = input("Please input the manager process's IP: ").strip()
+    
+        # Make sure that the IP address is correct.
+        try:
+            ipaddress.ip_address(ip_addr_manager)
+            break
+        except ValueError:
+            print("Invalid IP address input.")
+    
+    while True:
+        port_num_manager = int(input("Please input manager process's port: "))
 
+        # Make sure it is in the right port range.
+        if port_num_manager >= 13100 and port_num_manager <= 13199:
+            break
+        else:
+            print("Please use a port number in the range of 13100-13199.") 
+
+    message = input("Please type the command you want to send to the manager")
+    sock.sendto(message.encode('utf-8'), (ip_addr_manager, port_num_manager))
+
+    sock.close()
+    
 main()
