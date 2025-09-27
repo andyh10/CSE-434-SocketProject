5ebb02b5844a237db2126eef0814b2f545575f7c
Andy Huynh <115195444+andyh10@users.noreply.github.com>
2025-09-26 20:01:45 -0700

    Update client.py
    Added a peer port.

--- client.py
+++ client.py
@@ -3,9 +3,10 @@ import ipaddress
 import sys
 
 def main():
-    if len(sys.argv) != 4:
+    # Syntax check
+    if len(sys.argv) != 5:
         print("Please make sure you have the correct arguments.")
-        print("client.py <client_port> <server_ip> <server_port>")
+        print("client.py <client_port> <server_ip> <server_port> <peer_port>")
         exit(-1)
 
     if int(sys.argv[1]) < 13100 or int(sys.argv[1]) > 13199:
@@ -22,19 +23,28 @@ def main():
         print("Server Port - Please use a port number in the range of 13100-13199.")
         exit(-1)
 
+    if int(sys.argv[4]) < 13100 or int(sys.argv[4]) > 13199:
+        print("Peer Port - Please use a port number in the range of 13100-13199.")
+        exit(-1)
+
     # Create socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock_peer = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     # Bind to the local IP address and UDP port
-    sock.bind(('127.0.0.1', int(sys.argv[1])))
+    sock_server.bind(('127.0.0.1', int(sys.argv[1])))
+    sock_peer.bind(('127.0.0.1', int(sys.argv[4])))
 
     # Send data to server 
     message = input("Please type the command you want to send to the manager: ")
-    sock.sendto(message.encode("utf-8"), (sys.argv[2], int(sys.argv[3])))
+    sock_server.sendto(message.encode("utf-8"), (sys.argv[2], int(sys.argv[3])))
 
-    data, addr = sock.recvfrom(1024)
-    print(f"Server Response: {data.decode('utf-8')}")
+    # Wait for a server response
+    data, addr = sock_server.recvfrom(1024)
+    data_decoded = data.decode('utf-8')
 
-    sock.close()
+    print(f"Server Response: {data_decoded}")
+
+    sock_server.close()
     
 main()
