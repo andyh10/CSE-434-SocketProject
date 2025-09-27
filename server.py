39950d0a532dd108b4cb28e16ebf81fb4eab6b1c
Andy Huynh <115195444+andyh10@users.noreply.github.com>
2025-09-26 21:09:24 -0700

    Update server.py
    - Reformatted code
    - Print out the list for reg-user/reg-disk after every request sent
         - Add command count

--- server.py
+++ server.py
@@ -1,118 +1,159 @@
 import socket
+import ipaddress
 import sys
 
-def main():
-    # Create UDP socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+def handle_register_user(split, data):
+    # Syntax check
+    if len(split) != 5:
+        return "FAILURE: Please make sure you have the correct arguments."
+    
+    username = split[1]
+    ip = split[2]
+    mport = split[3]
+    cport = split[4]
 
+    if username in data:
+        return "FAILURE: User already in the list."
+    
+    try:
+        ipaddress.ip_address(ip)
+    except ValueError:
+        return "FAILURE: Invalid IP address for register-user-command."
+
+    for values in data.values():
+        if values["m-port"] == mport or values["c-port"] == cport:
+            return "FAILURE: Ports are already in use."
+        
+    data[username] = {
+        "ip": ip,
+        "m-port": mport,
+        "c-port": cport
+    }
+    return "SUCCESS"
+
+def handle_register_disk(split, data):
+    if len(split) != 5:
+        return "FAILURE: Please make sure you have the correct arguments."
+    
+    diskname = split[1]
+    ip = split[2]
+    mport = split[3]
+    cport = split[4]
+
+    if diskname in data:
+        return "FAILURE: Disk already in the list."
+    
+    for values in data.values():
+        if values["m-port"] == mport or values["c-port"] == cport:
+            return "FAILURE: Ports are already in use."
+        
+    data[diskname] = {
+        "ip": ip,
+        "m-port": mport,
+        "c-port": cport
+    }
+    return "SUCCESS"
+
+# TODO: Implement the handler for configuring dss after implementing client and disk to have both ports.
+def handle_configure_dss(split, addr):
+    return
+
+def deregister_user(username, data):
+    if username in data:
+        del data[username]
+        return "SUCCESS"
+    else:
+        return "FAILURE"
+
+# TODO Add check for disks in-use.  
+def deregister_disk(diskname, data):
+    if diskname in data:
+        del data[diskname]
+        return "SUCCESS"
+    else:
+        return "FAILURE"
+
+def main():
+    # Syntax Check
     if len(sys.argv) != 2:
-        print("Please input a port number in the cmd argument.")
+        print("Please make sure you have the correct arguments.")
+        print("server.py <server_port>")
         exit(-1)
 
     if int(sys.argv[1]) < 13100 or int(sys.argv[1]) > 13199:
         print("Please use a port number in the range of 13100-13199.")
         exit(-1)
 
+    # Create UDP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # Bind Socket
     sock.bind(('127.0.0.1', int(sys.argv[1])))
 
-    #Create Lists to Store 
+    print("Server is now online !")
+
+    # Dictionaries and ints to store data
     clients = {}
     disks = {}
     dss = {}
+    reg_user_req_count = 0
+    reg_disk_req_count = 0
+    con_dss_req_count  = 0
+    del_user_req_count = 0
+    del_disk_req_count = 0
 
-    #Handler Functions for register-user, register-disk, configure-dss
-    def handle_register_user(split, addr):
-        if len(split) != 5:
-            return "FAILURE: Please make sure you have the correct arguments."
-        
-        username = split[1]
-        ip = split[2]
-        mport = split[3]
-        cport = split[4]
-
-        if username in clients:
-            return "FAILURE: User already in the list."
-        
-        for values in clients.values():
-            if values["m-port"] == mport or values["c-port"] == cport:
-                return "FAILURE: Ports are already in use."
-            
-        clients[username] = {
-            "ip": ip,
-            "m-port": mport,
-            "c-port": cport
-        }
-        return "SUCCESS"
-    
-    def handle_register_disk(split, addr):
-        if len(split) != 5:
-            return "FAILURE: Please make sure you have the correct arguments."
-        
-        diskname = split[1]
-        ip = split[2]
-        mport = split[3]
-        cport = split[4]
-
-        if diskname in clients:
-            return "FAILURE: Disk already in the list."
-        
-        for values in disks.values():
-            if values["m-port"] == mport or values["c-port"] == cport:
-                return "FAILURE: Ports are already in use."
-            
-        disks[diskname] = {
-            "ip": ip,
-            "m-port": mport,
-            "c-port": cport
-        }
-        return "SUCCESS"
-    
-    #TO DO: Implement the handler for configuring dss after implementing client and disk to have both ports
-    def handle_configure_dss(split, addr):
-        return
-
-    def deregister_user(username):
-        if username in clients:
-            del clients[username]
-            return "SUCCESS"
-        else:
-            return "FAILURE"
-        
-    def deregister_disk(diskname):
-        if diskname in disks:
-            del disks[diskname]
-            return "SUCCESS"
-        else:
-            return "FAILURE"
-
-    # Data 
     while True:
         data, addr = sock.recvfrom(1024)
 
         #Allocate the message
         message = data.decode("utf-8").strip()
-
         split = message.split()
-
         if not split:
             continue
+
         command = split[0]
 
         #Handle Commands
         if command == "register-user":
-            handler = handle_register_user(split, addr)
+            reg_user_req_count += 1
+            handler = handle_register_user(split, clients)
+            response = handler.encode('utf-8')
+            print(f"Register-user command request number: {reg_user_req_count}")
+            print(f"Users registered is now: {clients}")
+            sock.sendto(response, addr)
+
         elif command == "register-disk":
-            handler = handle_register_disk(split, addr)
+            reg_disk_req_count += 1
+            handler = handle_register_disk(split, disks)
+            response = handler.encode('utf-8')
+            print(f"Register-disk command request number: {reg_disk_req_count}")
+            print(f"Disks registered is now: {disks}")
+            sock.sendto(response, addr)
+
         elif command == "configure-dss":
+            con_dss_req_count += 1
             handler = handle_configure_dss(split, addr)
+            print(f"Configure-dss command request number: {con_dss_req_count}")
+            response = handler.encode('utf-8')
+            sock.sendto(response, addr)
+
         elif command == "deregister-user":
+            del_user_req_count += 1
             username = split[1]
-            handler = deregister_user(username)
+            handler = deregister_user(username, clients)
+            print(f"Delete-user command request number: {del_user_req_count}")
+            response = handler.encode('utf-8')
+            sock.sendto(response, addr)
+
         elif command == "deregister-user":
+            del_disk_req_count += 1
             diskname = split[1]
-            handler = deregister_disk(diskname)
+            handler = deregister_disk(diskname, data)
+            print(f"Delete-disk command request number: {del_disk_req_count}")
+            response = handler.encode('utf-8')
+            sock.sendto(response, addr)
+
         else:
-            handler = "Invalid command."
+            handler = "Invalid command. Commands available: register-user, register-disk, or configure-dss."
 
 main()
-
